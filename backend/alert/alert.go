bb2c17f402b91bf5818888ce2808610f7cbf7ac0
dalhatmd <umarwalid4055@gmail.com>
2025-05-25 15:15:12 +0100

    Add alert package

---
+++ backend/alert/alert.go
@@ -0,0 +1,34 @@
+package alert
+
+import (
+	"time"
+)
+
+type Alert struct {
+	ID			string 		`gorm:"PrimaryKey" json: "id"`
+	ChildName	string 		`json: "child_name"`
+	Age			int			`json:"Age"`
+	Gender		string		`json: "gender"`
+	Description	string		`json: "description"`
+	LastSeenLocation	string	`json: "last_seen_location"`
+	PhotoUrl 	string		`json: "PhotoUrl"`
+	ReporterContact	string   `json: "reporter_contact"`
+	Status		string		 	`json: "status"`
+	CreatedAt	time.Time	  `json: "created_at"`
+	TimeLost	time.Time	  `json: "time_lost"`
+}
+
+func NewAlert(childName string, age int, last_seen_location string, description string, photoUrl string, reporterContact string) {
+	return &Alert{
+		ChildName:        childName,
+		Age:              age,
+		LastSeenLocation: last_seen_location,
+		Description:      description,
+		PhotoUrl:         photoUrl,
+		ReporterContact:  reporterContact,
+		Status:           "active",
+		CreatedAt:        time.Now(),
+	}
+}
+
+
---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/dalhatmd/Missing-Child-Alert
+
+go 1.24.0
